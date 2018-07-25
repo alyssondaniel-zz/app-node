6ed83321f7734303e498aad9a477678a1b4e70ed
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2018-07-25 13:25:59 -0300

    Adicionado mongodb

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -1,9 +1,19 @@
 describe('Routes: Products', () => {
+  let request;
+
+  before(()=> {
+    return setupApp()
+      .then(app => {
+        request = supertest(app)
+    })
+  });
+
   const defaultProduct = {
     name: 'Default product',
     description: 'product description',
     price: 100
   };
+
   describe('GET /products', () => {
     it('should return a list of products', done => {
       request.get('/products').end((err, res) => {
