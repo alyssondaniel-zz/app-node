dc26844e223abed2cac4cf8abef08b42b12303e0
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2018-07-25 15:13:18 -0300

    Integração entre rota, controller e model

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -5,7 +5,8 @@ class ProductsController {
 
   get(req, res) {
     return this.Product.find({})
-      .then(products => res.send(products));
+      .then(products => res.send(products))
+      .catch(err => res.status(400).send(err.message));
   }
 }
 
--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -1,3 +1,5 @@
+import Product from '../../../src/models/product';
+
 describe('Routes: Products', () => {
   let request;
 
@@ -14,12 +16,32 @@ describe('Routes: Products', () => {
     price: 100
   };
 
+  const expectedProduct = {
+    __v: 0,
+    _id: '56cb91bdc3464f14678934ca',
+    name: 'Default product',
+    description: 'product description',
+    price: 100
+  };
+
+  beforeEach(() => {
+    const product = new Product(defaultProduct);
+    product._id = '56cb91bdc3464f14678934ca';
+
+    return Product.remove({})
+      .then(() => product.save());
+  });
+
+  afterEach(() => Product.remove({}));
+
   describe('GET /products', () => {
     it('should return a list of products', done => {
-      request.get('/products').end((err, res) => {
-        expect(res.body[0]).to.eql(defaultProduct);
-        done(err);
-      })
+      request
+        .get('/products')
+        .end((err, res) => {
+          expect(res.body).to.eql([expectedProduct]);
+          done(err);
+        })
     });
   });
 });
